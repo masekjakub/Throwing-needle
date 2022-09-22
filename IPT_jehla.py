0b95c6586e436941d75baff5bf2bfe0ada92d8e5
masekjakub <masekjak02@gmail.com>
2022-09-22 20:47:31 +0200

    comments refactoring

--- IPT_jehla.py
+++ IPT_jehla.py
@@ -3,7 +3,9 @@ import math
 
 def isHit():
     for index in range(0,numberOfRows):
+        #check if starting point of needle is between rows
         if needleStartPoint > listOfRowsX[index] and needleStartPoint < listOfRowsX[index]+spaceBetweenRows:
+            #check if end of needle is in space between different rows
             if needleStartPoint+height < listOfRowsX[index] or needleStartPoint+height > listOfRowsX[index]+spaceBetweenRows:
                 return 1
     return 0
@@ -13,8 +15,8 @@ needleSize = 1000
 numberOfRows = 666
 numberOfThrows = 2000000
 
+hits=0
 listOfRowsX=[]
-hit=0
 paperSize = spaceBetweenRows*numberOfRows
 
 for row in range(0,numberOfRows):
@@ -23,11 +25,10 @@ for row in range(0,numberOfRows):
 for throw in range(0,numberOfThrows):
     needleStartPoint = random.randrange(0,paperSize)
     angle = random.randrange(1,360)
-    height = needleSize*math.sin(angle)
-
+    height = needleSize*math.sin(angle) #difference between starting point and end of needle
     if isHit():
-        hit=hit+1
+        hits=hits+1
 
-res = (2*needleSize*numberOfThrows)/(hit*spaceBetweenRows)
+res = (2*needleSize*numberOfThrows)/(hits*spaceBetweenRows)
 print(f"Calculated pi:{res}")
 print()
